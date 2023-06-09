faf66daf9fa60bf8afc8a61e2e3e4688d2d273f0
daw-18 <80912237+daw-18@users.noreply.github.com>
2023-06-10 01:55:30 +0200

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -14,12 +14,10 @@ public:
 	Interpreter_roman_nambers();
 	explicit Interpreter_roman_nambers(int){}
 	virtual ~Interpreter_roman_nambers(){}
+	virtual void interprer(string&,int&);
 	int interprer(string);
 
- 	virtual void interprer(string&,int&);
-
 protected:
-
 	virtual char one(){return 0;}
 	virtual string four(){return nullptr;}
 	virtual char five(){return 0;}
@@ -27,8 +25,6 @@ protected:
 	virtual int multipliter(){return 0;}	
 
 private:
-
-//wskazniki do jendostek aby muc szybko znajdowac funkcje
 	Interpreter_roman_nambers *thousand;
 	Interpreter_roman_nambers *hundred;
 	Interpreter_roman_nambers *ten;
@@ -42,7 +38,7 @@ public:
 	explicit Thousand(int) : Interpreter_roman_nambers(1){};
 
 protected:
-	char one() override {return 'M';}
+	 char one() override {return 'M';}
 	 string four() override {return "";}//brak M jest najwienksze
 	 char five() override {return '\0';}//sprawdzenie czy koniec lini 
 	 string nine() override {return "";}
@@ -56,7 +52,7 @@ public:
 	explicit Hundred(int) : Interpreter_roman_nambers(1){};
 
 protected:
-	char one() override {return 'C';}
+	 char one() override {return 'C';}
 	 string four() override {return "CD";}
 	 char five() override {return 'D';} 
 	 string nine() override {return "CM";}
@@ -69,7 +65,7 @@ public:
 	explicit Ten(int) : Interpreter_roman_nambers(1){};
 
 protected:
-	char one() override {return 'X';}
+	 char one() override {return 'X';}
 	 string four() override {return "XL";}
 	 char five() override {return 'L';} 
 	 string nine() override {return "XC";}
@@ -82,55 +78,69 @@ public:
 	explicit Unity(int) : Interpreter_roman_nambers(1){};
 
 protected:
-	char one() override {return 'I';}
+	 char one() override {return 'I';}
 	 string four() override {return "IV";}
 	 char five() override {return 'V';} 
 	 string nine() override {return "IX";}
 	 int multipliter() override {return 1;}
 };
 
+//////////////////////////////////////////////////////////////////////////////////////////
+
 Interpreter_roman_nambers::Interpreter_roman_nambers(){
+	//inicjuje fukcje pokolei
 	hundred=new Hundred(1);
 	thousand=new Thousand(1);
 	ten=new Ten(1);
 	unity=new Unity(1);}
 
+
+
 int Interpreter_roman_nambers::interprer(string number){
-	if(number.empty()){return 0;}
 	int total=0;
+
+	if(number.empty()){return 0;}//sprawdz czy ktoś coś podał
+	
 	thousand->interprer(number,total);
 	hundred->interprer(number,total);
 	ten->interprer(number,total);
 	unity->interprer(number,total);
+	
 	return total;}
 
+
+
 void Interpreter_roman_nambers::interprer(string &entry,int &result){
-		int index=0;
-		string test=entry.substr(0,2);
+	int index=0;
+	string test=entry.substr(0,2);
 		
-//sprawdznie najwarzniejszych liczb rzymskich
-		if(test==nine()){
-			result+=9*multipliter();
-			index+=2;}
-		else if(test==four()){
-			result+=4*multipliter();
-			index+=2;}
-		else{
-			if(entry[0]==five()){
-				result+=5;
-				index=1;}
-			else{index=0;}}
+	//sprawdznie najwarzniejszych liczb rzymskich
+	if(test==nine()){
+		result+=9*multipliter();
+		index+=2;}
+	else if(test==four()){
+		result+=4*multipliter();
+		index+=2;}
+	else{
+		if(entry[0]==five()){
+			result+=5;
+			index=1;}
+		else{index=0;}}
 
-//sprawdzaanie jensci ostatnich 3 cyfr,jesli przetem byLa specjalna 5 lub 4
-		for (int i = index+3; index<i;index++){
-			if(entry[index]==one()){result+=1*multipliter();}
-			else{break;}}
+	//sprawdzaanie jensci ostatnich 3 cyfr,jesli przetem byLa specjalna 5 lub 4
+	for (int i=index+3;index<i;index++){
+		if(entry[index]==one()){result+=1*multipliter();}
+		else{break;}}
 
-//wycinanie przerobionej liczby-paru liczb z zakresu 
+	//wycinanie przerobionej liczby-paru liczb z zakresu 
 	int length=entry.length()-index;
 	entry=entry.substr(index,length);}
 
+
+
+
 int main(int argc, char const *argv[]){
+	
 	Interpreter_roman_nambers interp;
 	string number="MMCXLVI";
 	cout<<"liczba rzymska  "<<number<<" to liczba dziesietna "<<interp.interprer(number);
