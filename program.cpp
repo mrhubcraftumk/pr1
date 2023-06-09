40ccb39c46a193c500db55d4a127e77486cd901f
daw-18 <80912237+daw-18@users.noreply.github.com>
2023-06-10 01:46:19 +0200

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 using namespace std;
-
+class Interpreter_roman_nambers;
 class Thousand;
 class Hundred;
 class Ten;
@@ -12,43 +12,19 @@ class Interpreter_roman_nambers{
 
 public:
 	Interpreter_roman_nambers();
-	explicit Interpreter_roman_nambers(int);
-	virtual ~Interpreter_roman_nambers();
+	explicit Interpreter_roman_nambers(int){}
+	virtual ~Interpreter_roman_nambers(){}
 	int interprer(string);
 
-	virtual void interprer(string &entry,int &result){
-		int index=0;
-		string test=entry.substr(0,2);
-		
-//sprawdznie najwarzniejszych liczb rzymskich
-		if(test==nine()){
-			wynik+=9*multipliter();
-			index+=2;}
-		else if(test==four()){
-			wynik+=4*multipliter();
-			index+=2;}
-		else{
-			if(entry[0]==five()){
-				wynik+=5;
-				index=1;}
-			else{index=0;}}
-
-//sprawdzaanie jensci ostatnich 3 cyfr,jesli przetem byLa specjalna 5 lub 4
-		for (int i = index+3; index<i;index++){
-			if(entry[index]==one()){wynik+==1*multipliter();}
-			else{break;}}
-
-//wycinanie przerobionej liczby-paru liczb z zakresu 
-	int length=entry.length()-index;
-	entry=entry.substr(index,length);}
+ 	virtual void interprer(string&,int&);
 
 protected:
 
 	virtual char one(){return 0;}
 	virtual string four(){return nullptr;}
 	virtual char five(){return 0;}
-	virtual nine(){return nullptr;}
-	virtual multipliter(){return 0;}	
+	virtual string nine(){return nullptr;}
+	virtual int multipliter(){return 0;}	
 
 private:
 
@@ -67,10 +43,10 @@ public:
 
 protected:
 	char one() override {return 'M';}
-	virtual string four() override {return "";}//brak M jest najwienksze
-	virtual char five() override {return '/0';}//sprawdzenie czy koniec lini 
-	virtual nine() override {return "";}
-	virtual multipliter() override {return 1000;}
+	 string four() override {return "";}//brak M jest najwienksze
+	 char five() override {return '\0';}//sprawdzenie czy koniec lini 
+	 string nine() override {return "";}
+	 int multipliter() override {return 1000;}
 	
 };
 
@@ -81,10 +57,10 @@ public:
 
 protected:
 	char one() override {return 'C';}
-	virtual string four() override {return "CD";}
-	virtual char five() override {return 'D';} 
-	virtual nine() override {return "CM";}
-	virtual multipliter() override {return 100;}
+	 string four() override {return "CD";}
+	 char five() override {return 'D';} 
+	 string nine() override {return "CM";}
+	 int multipliter() override {return 100;}
 };
 
 class Ten :public Interpreter_roman_nambers{
@@ -94,10 +70,10 @@ public:
 
 protected:
 	char one() override {return 'X';}
-	virtual string four() override {return "XL";}
-	virtual char five() override {return 'L';} 
-	virtual nine() override {return "XC";}
-	virtual multipliter() override {return 10;}
+	 string four() override {return "XL";}
+	 char five() override {return 'L';} 
+	 string nine() override {return "XC";}
+	 int multipliter() override {return 10;}
 };
 
 class Unity :public Interpreter_roman_nambers{
@@ -107,20 +83,20 @@ public:
 
 protected:
 	char one() override {return 'I';}
-	virtual string four() override {return "IV";}
-	virtual char five() override {return 'V';} 
-	virtual nine() override {return "IX";}
-	virtual multipliter() override {return 1;}
+	 string four() override {return "IV";}
+	 char five() override {return 'V';} 
+	 string nine() override {return "IX";}
+	 int multipliter() override {return 1;}
 };
 
 Interpreter_roman_nambers::Interpreter_roman_nambers(){
 	hundred=new Hundred(1);
 	thousand=new Thousand(1);
-	ten=new ten(1);
+	ten=new Ten(1);
 	unity=new Unity(1);}
 
-Interpreter_roman_nambers::interprer(string number){
-	if(liczba.empty()){return 0;}
+int Interpreter_roman_nambers::interprer(string number){
+	if(number.empty()){return 0;}
 	int total=0;
 	thousand->interprer(number,total);
 	hundred->interprer(number,total);
@@ -128,11 +104,36 @@ Interpreter_roman_nambers::interprer(string number){
 	unity->interprer(number,total);
 	return total;}
 
-int main(int argc, char const *argv[])
-{
-	Interpreter_roman_nambers interprer;
-	string number="MMCXLVI"
-	cout<<"liczba rzymska  "<<number<<" to liczba dziesietna "<<interprer.interprer(number);
-	
+void Interpreter_roman_nambers::interprer(string &entry,int &result){
+		int index=0;
+		string test=entry.substr(0,2);
+		
+//sprawdznie najwarzniejszych liczb rzymskich
+		if(test==nine()){
+			result+=9*multipliter();
+			index+=2;}
+		else if(test==four()){
+			result+=4*multipliter();
+			index+=2;}
+		else{
+			if(entry[0]==five()){
+				result+=5;
+				index=1;}
+			else{index=0;}}
+
+//sprawdzaanie jensci ostatnich 3 cyfr,jesli przetem byLa specjalna 5 lub 4
+		for (int i = index+3; index<i;index++){
+			if(entry[index]==one()){result+=1*multipliter();}
+			else{break;}}
+
+//wycinanie przerobionej liczby-paru liczb z zakresu 
+	int length=entry.length()-index;
+	entry=entry.substr(index,length);}
+
+int main(int argc, char const *argv[]){
+	Interpreter_roman_nambers interp;
+	string number="MMCXLVI";
+	cout<<"liczba rzymska  "<<number<<" to liczba dziesietna "<<interp.interprer(number);
+
 	return 0;
 }
